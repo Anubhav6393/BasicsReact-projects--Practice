877e96461af3500a2ac492416b27b5098990d711
Anubhav Mishra <mishraanubhav8126@gmail.com>
2024-06-30 22:41:23 +0530

    updates

--- 07reactRouter/src/Components/Github/Github.jsx
+++ 07reactRouter/src/Components/Github/Github.jsx
@@ -26,4 +26,4 @@ export default Github;
 export const githubInfoLoader = async () => {
   const response = await fetch("https://api.github.com/users/Anubhav6393");
   return response.json();
-};
+}; 
